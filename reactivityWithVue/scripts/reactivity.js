368f7086dafaa44ae4237087690fa20508d37999
iWill <alxs1dd93@gmail.com>
2025-03-11 20:50:44 +0700

    computed

--- reactivityWithVue/scripts/reactivity.js
+++ reactivityWithVue/scripts/reactivity.js
@@ -1,31 +1,29 @@
-import { ref, watchEffect, watch } from 'vue'
+import { ref, watchEffect, computed } from 'vue'
 
 const counterBtn = document.querySelector('.counter')
 const resetBtn = document.querySelector('.reset')
 
 const counterState = ref(5)
+const isCounterTooBig = computed(() => counterState > 10)
 
 const renderCounter = () => {
     counterBtn.textContent = `Count: ${counterState.value}`
-    counterBtn.classList.toggle('red', isCounterTooBig())
+    counterBtn.classList.toggle('red', isCounterTooBig.value)
 };
 
 watchEffect(() => {
-    renderCounter()
+    renderCounter();
 });
 
-function isCounterTooBig() {
-    return counterState.value > 10
-}
 
 counterBtn.addEventListener('click', () => {
     counterState.value++;
-})
+});
 
 resetBtn.addEventListener('click', () => {
     counterState.value = 0;
-})
+});
 
 setInterval(() => {
     counterState.value++;
-}, 1000)
+}, 1000);
