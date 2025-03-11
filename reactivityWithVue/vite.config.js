24148df39f1a40c3669f52aba5d689ae9be94478
iWill <alxs1dd93@gmail.com>
2025-03-11 20:13:36 +0700

    swap port

--- reactivityWithVue/vite.config.js
+++ reactivityWithVue/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   publicDir: 'public',
+  server: {
+    port: 3006,
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
