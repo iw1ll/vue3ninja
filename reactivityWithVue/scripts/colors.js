989cb66dbeb000bdc393173fd166911f8bf4478e
iWill <alxs1dd93@gmail.com>
2025-03-11 22:32:18 +0700

    add node + style

--- reactivityWithVue/scripts/colors.js
+++ reactivityWithVue/scripts/colors.js
@@ -8,10 +8,34 @@ const colors = ref([
     { r: 0, g: 0, b: 255 },
 ]);
 
-document.querySelector('button#add').addEventListener('click', () => {
+watchEffect(() => {
+    const container = document.querySelector('#colors');
+    container.innerHTML = '';
+    
+    console.log(container)
+    colors.value.forEach(c => {
+        const node = document.createElement('div');
+    
+        node.classList.add('color');
+        node.style.backgroundColor = `rgb(${c.r}, ${c.g}, ${c.b})`;
+        node.style.borderColor = `rgb(${c.r * 0.8}, ${c.g * 0.8 }, ${c.b * 0.8})`;
+        container.appendChild(node);
+    });
+})
 
+document.querySelector('button#add').addEventListener('click', () => {
+    colors.value.push(
+        { 
+            r: Math.random() * 255,
+            g: Math.random() * 255,
+            b: Math.random() * 255,
+        },
+    )
 });
 
-document.querySelector('button#add').addEventListener('click', () => {
-    
+
+
+document.querySelector('button#reset').addEventListener('click', () => {
+    const container = document.querySelector('#colors');
+    container.innerHTML = '';
 });
