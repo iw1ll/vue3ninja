410d5bdcd09bc9ac282a8478f565b5e896476d82
iWill <alxs1dd93@gmail.com>
2025-03-12 00:02:26 +0700

    new color

--- reactivityWithVue/scripts/colors.js
+++ reactivityWithVue/scripts/colors.js
@@ -1,7 +1,7 @@
 import { ref, watchEffect } from 'vue';
 import { shuffle } from 'lodash';
-const SIZE = 60;
 
+const SIZE = 60;
 const colors = ref([
     { r: 255, g: 0, b: 0 },
     { r: 0, g: 255, b: 0 },
@@ -10,36 +10,38 @@ const colors = ref([
 
 watchEffect(() => {
     const container = document.querySelector('#colors');
-    container.innerHTML = '';
-    
-    console.log(container);
+
+    if (colors.value.length === 0) {
+        container.innerHTML = '';
+        return;
+    }
+
     colors.value.forEach((c, i) => {
-        const node = document.createElement('div');
+        const color = `rgb(${c.r}, ${c.g}, ${c.b})`;
+
+        let node = container.querySelector(`.color[id="${color}"]`);
+        
+        if (!node) {
+            node = document.createElement('div');
+            node.classList.add('color');
+            node.id = color;
+            node.style.backgroundColor = color;
+            node.style.borderColor = `rgb(${c.r * 0.8}, ${c.g * 0.8 }, ${c.b * 0.8})`;
+            container.appendChild(node);
+        }
     
-        node.classList.add('color');
-        node.style.backgroundColor = `rgb(${c.r}, ${c.g}, ${c.b})`;
-        node.style.borderColor = `rgb(${c.r * 0.8}, ${c.g * 0.8 }, ${c.b * 0.8})`;
         node.style.transform = `translateX(${SIZE * i}px)`;
-        container.appendChild(node);
     });
 });
 
 document.querySelector('button#add').addEventListener('click', () => {
-    if (colors.value.length === 0) {
-        colors.value.push(
-            { r: 255, g: 0, b: 0 },
-            { r: 0, g: 255, b: 0 },
-            { r: 0, g: 0, b: 255 },
-        );
-    } else {
-        colors.value.push(
-            { 
-                r: Math.random() * 255,
-                g: Math.random() * 255,
-                b: Math.random() * 255,
-            },
-        );
-    }
+    colors.value.push(
+        { 
+            r: Math.random() * 255,
+            g: Math.random() * 255,
+            b: Math.random() * 255,
+        },
+    );
 });
 
 document.querySelector('button#reset').addEventListener('click', () => {
