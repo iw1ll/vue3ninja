aba9c123bd50918b3de0f8bda3803348d6d99b93
iWill <alxs1dd93@gmail.com>
2025-03-11 23:07:55 +0700

    fun clear

--- reactivityWithVue/scripts/colors.js
+++ reactivityWithVue/scripts/colors.js
@@ -13,24 +13,33 @@ watchEffect(() => {
     container.innerHTML = '';
     
     console.log(container)
-    colors.value.forEach(c => {
+    colors.value.forEach((c, i) => {
         const node = document.createElement('div');
     
         node.classList.add('color');
         node.style.backgroundColor = `rgb(${c.r}, ${c.g}, ${c.b})`;
         node.style.borderColor = `rgb(${c.r * 0.8}, ${c.g * 0.8 }, ${c.b * 0.8})`;
+        node.style.transform = `translateX(${SIZE * i}px)`
         container.appendChild(node);
     });
 })
 
 document.querySelector('button#add').addEventListener('click', () => {
-    colors.value.push(
-        { 
-            r: Math.random() * 255,
-            g: Math.random() * 255,
-            b: Math.random() * 255,
-        },
-    )
+    if (colors.value.length === 0) {
+        colors.value.push(
+            { r: 255, g: 0, b: 0 },
+            { r: 0, g: 255, b: 0 },
+            { r: 0, g: 0, b: 255 },
+        )
+    } else {
+        colors.value.push(
+            { 
+                r: Math.random() * 255,
+                g: Math.random() * 255,
+                b: Math.random() * 255,
+            },
+        )
+    }
 });
 
 document.querySelector('button#reset').addEventListener('click', () => {
