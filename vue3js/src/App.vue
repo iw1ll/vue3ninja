741121e985a0a5ef8d852644de9439c3fa358404
iWill <alxs1dd93@gmail.com>
2025-03-16 10:57:00 +0700

    dirty vue

--- vue3js/src/App.vue
+++ vue3js/src/App.vue
@@ -1,11 +1,50 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router';
-import HelloWorld from './components/HelloWorld.vue';
+// import { RouterLink, RouterView } from 'vue-router';
+// import HelloWorld from './components/HelloWorld.vue';
+import { ref } from 'vue';
+
+const SIZE = 60;
+
+const randomValue = ref<number>(0);
+const a = ref<string>('a');
+
+const colors = ref([
+    { r: 255, g: 0, b: 0 },
+    { r: 0, g: 255, b: 0 },
+    { r: 0, g: 0, b: 255 },
+]);
+
+function addColor() {
+    colors.value.push({
+        r: Math.random() * 255,
+        g: Math.random() * 255,
+        b: Math.random() * 255,
+    });
+};
+
+function resetColors() {
+    colors.value = [];
+};
+
+function shuffle(arr: any) {
+    return arr.toSorted(() => (Math.random() < .5 ? 1 : -1));
+}
+
+function shuffleColors() {
+    colors.value = shuffle(colors.value);
+};
+
+function randomNum() {
+    randomValue.value =  Math.floor(Math.random() * 100);
+    a.value = a.value + ' kek';
+}
+
+
 </script>
 
 <template>
     <header>
-        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+        <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
         <div class="wrapper">
           
@@ -15,13 +54,47 @@ import HelloWorld from './components/HelloWorld.vue';
                 <RouterLink to="/">Home</RouterLink>
                 <RouterLink to="/about">About</RouterLink>
             </nav>
-        </div>
+        </div> -->
     </header>
+    <div id="colors">
+        <div class="color" v-for="(c, i) in colors" :style="{
+            backgroundColor: `rgb(${c.r}, ${c.g}, ${c.b})`,
+            borderColor: `rgb(${c.r * 0.8}, ${c.g * 0.8}, ${c.b * 0.8})`,
+            transform: `translateX(${SIZE * i}px)`
+        }" :key="`rgb(${c.r}, ${c.g}, ${c.b})`"></div>
 
-    <RouterView />
+    </div>
+    <input placeholder="a" v-model="a"/>
+    <button id="add" v-if="colors.length < 5" @click="addColor">Add</button>
+    <button id="reset" @click="resetColors">Reset</button>
+    <button id="shuffle" @click="shuffleColors">Shuffle</button>
+    <button @click="randomNum()">{{ randomValue }}</button>
+    <!-- <RouterView /> -->
 </template>
 
 <style scoped>
+#colors {
+  border: solid lightblue 1px;
+  height: 60px;
+  position: relative;
+  margin-bottom: 5px;
+}
+
+.color {
+  top: 0;
+  left: 0;
+  box-sizing: border-box;
+  border-style: solid;
+  border-width: 4px;
+  display: inline-block;
+  height: 60px;
+  width: 60px;
+  border-radius: 5px;
+
+  position: absolute;
+  transition: all 0.5s;
+}
+
 .app-container {
   max-width: 1280px;
   margin: 0 auto;
