17ec1b0ac7e50c10d8003b1d838a1698c1f8c941
iWill <alxs1dd93@gmail.com>
2025-03-20 02:01:00 +0700

    fiz

--- vue3js/src/components/CountNum.vue
+++ vue3js/src/components/CountNum.vue
@@ -2,14 +2,11 @@
 defineProps<{
     count? : number
 }>();
-function inc() {
-//  count++
-}
+
 </script>
 
 <template>
     <div class="greetings">
         <h1 class="green">{{ count }}</h1>
-        <button @click="inc"></button>
     </div>
 </template>
